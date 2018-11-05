2aec1eeeb56e76f1bd5e98ca21ff7bcb1840019b
Your Name <you@example.com>
2018-11-05 12:54:31 +0100

    Making divs for content

--- shoppingCart.js
+++ shoppingCart.js
@@ -1,4 +1,5 @@
 var listOfProducts;
+var shoppingCartJson;
 
 function loadProducts() {
     fetch("./products.json")
@@ -11,16 +12,17 @@ function loadProducts() {
     });
 }
 
-function initShoppingCart() {
 
-    var shoppingCartString = localStorage.getItem("shoppingCart");
-    /*var shoppingCartJson = JSON.parse(shoppingCartString);*/
+function initShoppingCart() {
+    shoppingCartString = localStorage.getItem("shoppingCart");
+    shoppingCartJson = JSON.parse(shoppingCartString);
     if (!("shoppingCart" in localStorage)) {
         localStorage.setItem("shoppingCart", "[]");
     }
     /*loadProducts();*/
 
     /*addProductsToWebpage(shoppingCartJson);*/
+    ProductsInKundvagnWebPage(listOfProducts);
     updateNumberOfChosenProducts();
 }
 
@@ -31,3 +33,28 @@ function updateNumberOfChosenProducts() {
     var shoppingCartJson = JSON.parse(shoppingCartString);
     productNumberIndicator.innerText = shoppingCartJson.length;
 }
+
+function ProductsInKundvagnWebPage (shoppingCartJson) {
+   
+    //var ProductInCart = localStorage.getItem("shoppingCart");
+
+    var divForTheProductInKundvagn = document.createElement("div");
+
+ for(var i = 0; i < shoppingCartJson.length; i++) {
+        divForTheProduct = document.createElement("div");
+        productName = productDiv(shoppingCartJson[i]);
+    }
+
+    divForTheProductInKundvagn.appendChild(divForTheProduct);
+
+    
+
+}
+
+function productDiv() {
+    var oneProductDiv = document.createElement("div");
+    oneProductDiv.classList.add("d-flex", "flex-row", "justify-content-center");
+    return oneProductDiv;
+}
+
+    var ProductInCart = localStorage.getItem("shoppingCart");
